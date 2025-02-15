63e0b6b3ce2b84832ca67a992443047940295d76
Alexandre Dos Reis <ajm.dosreis.daponte@gmail.com>
2025-02-15 15:22:36 +0100

    handling sort, range and filters

--- handlers/adminHandlers.go
+++ handlers/adminHandlers.go
@@ -2,8 +2,10 @@ package handlers
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/jackc/pgx/v5"
 	"github.com/labstack/echo/v4"
@@ -33,9 +35,14 @@ func (req *GetListRequest) bind(c echo.Context) (*GetListRequest, error) {
 	if err := BindValue(c.QueryParam("sort"), &req.Sort); err != nil {
 		return nil, err
 	}
+
 	if err := BindValue(c.QueryParam("range"), &req.Range); err != nil {
 		return nil, err
 	}
+	if len(req.Range) < 2 {
+		return nil, errors.New("Invalid range parameter")
+	}
+
 	if err := BindValue(c.QueryParam("filter"), &req.Filter); err != nil {
 		return nil, err
 	}
@@ -67,22 +74,58 @@ func (h *Handlers) adminHandlers() {
 			return c.JSON(http.StatusBadRequest, "bad request")
 		}
 
-		// check if table exist in struct
+		start, end := request.Range[0], request.Range[1]
+		limit := end - start + 1
+		offset := start
+
+		orderBy := ""
+		if len(request.Sort) == 2 {
+			column, direction := request.Sort[0], request.Sort[1]
+			if direction != "ASC" && direction != "DESC" {
+				return errors.New("invalid sort direction")
+			}
+			orderBy = fmt.Sprintf("ORDER BY %s %s", column, direction)
+		}
+
+		// Build filtering query part
+		var filterConditions []string
+		for column, value := range request.Filter {
+			filterConditions = append(filterConditions, fmt.Sprintf("%s = '%s'", column, value))
+		}
+		filterQuery := ""
+		if len(filterConditions) > 0 {
+			filterQuery = "WHERE " + strings.Join(filterConditions, " AND ")
+		}
+
+		// TODO: use a transaction to wrap data and count
 		query := fmt.Sprintf(`
 		SELECT row_to_json(r) FROM (
-			SELECT * FROM %s
-		) r;`, request.Resource)
+			SELECT * FROM %s %s %s LIMIT %d OFFSET %d
+		) r;`, request.Resource, filterQuery, orderBy, limit, offset)
 
 		rows, err := h.db.Query(h.ctx, query)
 		if err != nil {
-			return c.JSON(http.StatusBadRequest, "Request malformated !")
+			return errors.New("Request malformated !")
 		}
 		defer rows.Close()
 
-		res := pgJsonToArrayMap(rows)
-		len := len(res)
+		countQuery := fmt.Sprintf("SELECT COUNT(*) FROM %s %s;", request.Resource, filterQuery)
+		var total int
+		totalErr := h.db.QueryRow(h.ctx, countQuery).Scan(&total)
+		if totalErr != nil {
+			return errors.New("failed to count records")
+		}
 
-		contentRange := fmt.Sprintf("%s %d-%d/%d", request.Resource, 0, len, len)
+		res := pgJsonToArrayMap(rows)
+		lenRows := len(res)
+
+		contentRange := fmt.Sprintf(
+			"%s %d-%d/%d",
+			request.Resource,
+			start,
+			start+lenRows-1,
+			total,
+		)
 		c.Response().Header().Set("Content-Range", contentRange)
 		c.Response().Header().Set("Access-Control-Expose-Headers", "Content-Range")
 		return c.JSON(http.StatusOK, res)
